f35522cb3ac982ac4d122487a44a7eae4cdb3252
chas-luftmint <armin.ozkalp@chasacademy.se>
2026-02-10 10:11:12 +0100

    index fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 if (require.main == module) {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${PORT}`);
 });
 }
 
